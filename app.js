65c7bfb36a7283770d5f8d6c695264dbe2d3bb46
asx-dev <asxdev18@gmail.com>
2025-01-31 15:36:45 +0000

    Added helmet js

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const shopRoutes = require("./routes/shopRoute");
+const helmet = require("helmet");
 
 // Middlewares
 app.use(cors());
 app.use(express.json());
-
+app.use(helmet());
 // Routes
 app.use("/api/shop", shopRoutes);
 
